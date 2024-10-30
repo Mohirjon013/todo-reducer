b02dc2aa76af59b8e6c5ba55e10db5f226e00505
Mohirjon013 <mohirjon26@gmail.com>
2024-10-30 16:36:43 +0100

    change

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,39 +1,41 @@
 import { DeleteOutlined, EditOutlined, HeartOutlined, SaveOutlined } from '@ant-design/icons'
 import { Button, Input } from 'antd'
-import React, { useReducer, useState } from 'react'
+import React, { act, useReducer, useState } from 'react'
 import HeartIcon from "../assets/images/heart.svg"
 import Saved from "../assets/images/saved.svg"
-import Item from 'antd/es/list/Item'
+import Modal from './Modal'
 
 
 const TYPES = {
     create:"CREATE",
     liked:"LIKED",
     delete:"DELETE",
-    saved: "SAVE"
+    saved: "SAVE",
+    edit:"UPDATE"
 }
 function reducer(state, action){
     if(action.type == TYPES.create){
         return {
             todos:[...state.todos, action.payload],
-            likedList:state.likedList
+            likedList:state.likedList,
+            savedList:state.savedList
         }
     }
     else if(action.type == TYPES.liked){
-
+        const FindLike = state.todos.map(item => item.id === action.payload.id ? {...item, isLiked: !item.isLiked} : item)
         return {
-            todos:state.todos,
-            // likedList:[...state.likedList, action.payload]
-            todos:state.todos.map(item => item.id === action.payload.id ? {...item, isLiked: !item.isLiked} : item)
+            todos:FindLike,
+            likedList: FindLike.filter(item => item.isLiked),
+            savedList:state.savedList
         }
         
     }
     else if(action.type == TYPES.saved){
-
+        const FindSaved = state.todos.map(item => item.id === action.payload.id ? {...item, isSaved: !item.isSaved} : item)
         return {
-            todos:state.todos,
-            todos:state.todos.map(item => item.id === action.payload.id ? {...item, isSaved: !item.isSaved} : item)
-
+            todos:FindSaved,
+            likedList:state.likedList,
+            savedList:FindSaved.filter(item => item.isSaved)
         }
         
     }
@@ -42,16 +44,28 @@ function reducer(state, action){
         state.todos.splice(deleteIndex, 1)
         return {
             todos:[...state.todos],
-            likedList:state.likedList
+            likedList:state.likedList,    
+            savedList:state.savedList
+        }
+    }
+    else if(action.type == TYPES.edit){
+        return {
+            todos:[...state.todos],
+            likedList:state.likedList,
+            savedList:state.savedList
         }
     }
 }
 
 const intialState = {
     todos:[],
-    likedList:[]
+    likedList:[],
+    savedList:[]
 }
 function Todos() {
+    const [updateDate, setUpdateDate] = useState(null)
+    const [updateId, setUpdateId] = useState(null)
+    const [openModal, setOpenModal] = useState(false)
     const [data, dispatch] = useReducer(reducer, intialState)
     const [inputValue, setInputValue] = useState("")
     function handleSubmit(e){
@@ -67,7 +81,22 @@ function Todos() {
         setInputValue("")
     }
 
-    
+    function handleSubmitBtnClik(id){
+        setOpenModal(true)
+        const findData = data.todos.find(item => item.id === id)
+        
+        setUpdateId(id)
+        setUpdateDate(findData.value)
+        console.log(findData);
+        
+    }
+    function handleEditBtn(e){
+        e.preventDefault()
+        const findData = data.todos.find(item => item.id === updateId)
+        findData.value = updateDate
+        dispatch({type:TYPES.edit})
+        setOpenModal(false)
+    }
   return (
     <>
 
@@ -76,7 +105,7 @@ function Todos() {
             <ul className='flex pr-5 gap-5'>
                 <li className='w-[60px] border-[1px] border-black rounded-full cursor-pointer'>
                     <button className='flex items-center gap-1 pl-1'>
-                        <span>{data.todos.filter(item => item.isLiked).length}</span>
+                        <span>{data.likedList.length}</span>
                         <img className='mx-auto py-3' src={HeartIcon} alt="heart img" width={30} height={30} />
                     </button>
                 </li>
@@ -90,7 +119,7 @@ function Todos() {
             </ul>
         </div>
         <form onSubmit={handleSubmit} autoComplete='off' className='w-[500px] mx-auto flex items-center justify-between bg-white p-5 rounded-md mt-10'>
-            <Input onChange={(e) => setInputValue(e.target.value)} name='todo' className='w-[80%]' placeholder='Add Todo' size='large' allowClear />
+            <Input value={inputValue} onChange={(e) => setInputValue(e.target.value)} name='todo' className='w-[80%]' placeholder='Add Todo' size='large' allowClear />
             <Button htmlType='submit' type='primary' size='large'>Submit</Button>
         </form>
         <ul className='w-[500px] bg-white mt-5 p-5 mx-auto rounded-md space-y-3'>
@@ -104,11 +133,18 @@ function Todos() {
                         </button>
                         <button onClick={() => dispatch({type:TYPES.saved, payload:item})} className={`${item.isSaved ? "text-green-500" : ""}`} > <SaveOutlined/> </button>
                         <button onClick={() => dispatch({type:TYPES.delete, payload:item.id})}> <DeleteOutlined/> </button>
-                        <button> <EditOutlined/> </button>
+                        <button onClick={() => handleSubmitBtnClik(item.id)}> <EditOutlined/> </button>
                     </div>
                 </li>
             ))}
         </ul>
+        <Modal openModal={openModal} setOpenModal={setOpenModal} >
+            <form onSubmit={handleEditBtn} className="flex items-center gap-2 justify-between">
+                <Input value={updateDate} onChange={(e) => setUpdateDate(e.target.value)} name='todo' className='w-[80%]' placeholder='Add Todo' size='large' allowClear />
+                <Button   className='w-[20%]' htmlType='submit' type='primary' size='large'>Edit</Button>
+            </form>
+
+        </Modal>
     </>
   )
 }
